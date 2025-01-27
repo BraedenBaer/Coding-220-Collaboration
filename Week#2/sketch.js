3232d5ced335afd55893d52cfd4ceca77c6193cb
BraedenBaer <98059344+BraedenBaer@users.noreply.github.com>
2025-01-26 17:00:16 -0700

    Add files via upload

---
+++ Week#2/sketch.js
@@ -0,0 +1,74 @@
+llink to piece
+https://editor.p5js.org/
+
+This is what chat GPT came up with for a pizza made with code:
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(255);
+
+  // Draw a brown circle for the pizza base
+  fill(160, 82, 45);
+  ellipse(200, 200, 300, 300);
+
+  // Draw red circles for tomato sauce
+  fill(255, 0, 0);
+  ellipse(150, 150, 100, 100);
+  ellipse(250, 250, 100, 100);
+
+  // Draw yellow circles for cheese
+  fill(255, 255, 0);
+  ellipse(180, 220, 80, 80);
+  ellipse(220, 180, 80, 80);
+}
+
+
+
+
+This is what I made for a pizza made with code:
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(255);
+
+  //  brown circle for the pizza base
+  fill(160, 82, 45);
+  ellipse(200, 200, 400, 400);
+
+  // one big red circle for tomato sauce
+  fill(855, 10, 10);
+  ellipse(200, 200, 350, 350);
+  
+  // a yellow circle for cheese
+  fill(255, 255, 0);
+  ellipse(200, 200, 325, 325);
+  
+  // red circles for pepperoni
+  fill (855, 10, 10);
+  ellipse(125, 100, 50, 50);
+  ellipse(150, 180, 50, 50);
+  ellipse(80, 180, 50, 50);
+  ellipse(100, 250, 50, 50);
+  ellipse(320, 230, 50, 50);
+  ellipse(250, 150, 50, 50);
+  ellipse(220, 70, 50, 50);
+  ellipse(180, 290, 50, 50);
+  
+  // green circles for olives
+  fill(50, 200 , 10)
+  ellipse( 220, 150, 25, 25)
+  ellipse( 300, 200, 25, 25)
+  ellipse( 220, 100, 25, 25)
+  ellipse( 250, 330, 25, 25)
+  ellipse( 100, 150, 25, 25)
+  ellipse( 250, 250, 25, 25)
+  ellipse( 340, 150, 25, 25)
+}
+Looking back at the two pizzas I believe mine is the more superier piece. While Chat GPT new how to make the shapes and color theingredients of the pizza
+yet when it came down to positioning and editing sizes to make thispiece look like it was made by a person and not some ai. That being said Chat GPT will deffinitly 
+be a lot of help for the reminder of this course.
+  
